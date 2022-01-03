a60d16dd1e01d67ac08e1b2fd178126c3ce02b47
Emeu <marie.le-du@epitech.eu>
2022-01-03 09:18:29 +0100

    test

---
+++ hacker-stories/src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+
+    const title = "React";
+
+  return (
+    <div className="App">
+      <h1>Hello {title}</h1>
+        <label htmlFor={"search"}>Search :</label>
+        <input id={"search"} type={"text"}/>
+    </div>
+  );
+}
+
+export default App;
